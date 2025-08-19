10f180b1e3fdbb7f43cbbeae1192c87f5b5c3222
Seu Nome <seu@email.com>
2025-08-18 22:49:10 -0300

    us

--- Backend/Main.py
+++ Backend/Main.py
@@ -41,9 +41,6 @@ def create_app():
 
     return app
 
-
-if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    if __name__ == "__main__":
+     app = create_app()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
